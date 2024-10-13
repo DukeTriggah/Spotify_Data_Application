8e89f3f654d54e5dbcf25a754b245abe9a9e4cde
DukeTriggah <ntoutoume36@gmail.com>
2024-10-13 11:47:49 -0400

    Created dirrectories for flask application and django application

---
+++ test.py
@@ -0,0 +1,10 @@
+import requests
+# Import the data manipulation libraries
+# Maybe get AI library (Watson IBM)
+# Get spotify API
+# Import Flask for scket use (suggestion, may not be used)
+
+def fetchdata():
+    response = requests.get()
+    response.raise_for_status()
+    print()
